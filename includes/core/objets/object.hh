1dedb3c00d7ec16652d297afbfd3e9e81297e68e
Damien DIDIER <damien.didier@epita.fr>
2023-08-08 00:11:00 +0200

    Object transformations:
    Get translation, rotation, scale and overhaul transformation matrices.

--- includes/core/objets/object.hh
+++ includes/core/objets/object.hh
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <glm/ext/quaternion_float.hpp>
-#include <glm/ext/vector_float3.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
 
 namespace Playground::Core
 {
@@ -15,19 +15,54 @@ namespace Playground::Core
         glm::vec3 scale_;
 
     public:
-        Object() = default;
+        Object();
+        Object(glm::vec3 position, glm::quat rotation, glm::vec3 scale);
 
-        void setPosition(glm::vec3 &position);
-        glm::vec3 getPosition();
+        // Position
+    public:
+        void setPosition(glm::vec3 const &position);
+        const glm::vec3 getPosition();
 
-        void move(glm::vec3 &other);
+        void move(glm::vec3 const &other);
 
-        void setRotation(glm::quat &rotation);
-        void setRotation(glm::vec3 &euler_rotation);
-        glm::quat getRotation();
+        const glm::mat4 getTranslationMatrix();
+        const glm::mat4 getInverseTranslationMatrix();
 
-        void setScale(glm::vec3 &scale);
-        glm::vec3 getScale();
+        // Rotation
+    public:
+        void setRotation(glm::quat const &rotation);
+        void setRotation(glm::vec3 const &euler_angles);
+        const glm::quat getRotation();
+
+        const glm::mat4 getRotationMatrix();
+        const glm::mat4 getInverseRotationMatrix();
+
+        // Scale
+    public:
+        void setScale(glm::vec3 const &scale);
+        const glm::vec3 getScale();
+
+        const glm::mat4 getScaleMatrix();
+        const glm::mat4 getInverseScaleMatrix();
+
+        // Transformation Matrix
+    public:
+        const glm::mat4 getTransformMatrix();
+        const glm::mat4 getInverseTransformMatrix();
+
+        // Utils
+    public:
+        const glm::vec3 getRight();
+        const glm::vec3 getFront();
+        const glm::vec3 getUp();
+
+        //         // Rendering
+        //     protected:
+        //         virtual void preRender() = 0;
+        //         virtual void postRender() = 0;
+        //
+        //     public:
+        //         virtual void render() = 0;
     };
 
 } // namespace Playground::Core
--- src/core/objects/object.cc
+++ src/core/objects/object.cc
@@ -1,49 +1,98 @@
 #include "core/objets/object.hh"
 
-#include <glm/ext/quaternion_float.hpp>
-#include <glm/ext/vector_float3.hpp>
-
 namespace Playground::Core
 {
 
-    void Object::setPosition(glm::vec3 &position)
+    Object::Object()
+        : position_(0, 0, 0)
+        , rotation_(1.0f, 0.0f, 0.0f, 0.0f)
+        , scale_(1.0f)
+    {}
+
+    // Position
+    void Object::setPosition(glm::vec3 const &position)
     {
         position_ = position;
     }
 
-    glm::vec3 Object::getPosition()
+    const glm::vec3 Object::getPosition()
     {
         return position_;
     }
 
-    void Object::move(glm::vec3 &other)
+    void Object::move(glm::vec3 const &other)
     {
         position_ += other;
     }
 
-    void Object::setRotation(glm::quat &rotation)
+    const glm::mat4 Object::getTranslationMatrix()
+    {
+        return glm::translate(glm::mat4(1.0f), position_);
+    }
+
+    const glm::mat4 Object::getInverseTranslationMatrix()
+    {
+        return glm::translate(glm::mat4(1.0f), -position_);
+    }
+
+    // Rotation
+    void Object::setRotation(glm::quat const &rotation)
     {
         rotation_ = rotation;
     }
 
-    void Object::setRotation(glm::vec3 &euler_rotation)
+    void Object::setRotation(glm::vec3 const &euler_angle)
     {
-        rotation_ = glm::quat(euler_rotation);
+        rotation_ = glm::quat(euler_angle);
     }
 
-    glm::quat Object::getRotation()
+    const glm::quat Object::getRotation()
     {
         return rotation_;
     }
 
-    void Object::setScale(glm::vec3 &scale)
+    const glm::mat4 Object::getRotationMatrix()
+    {
+        return glm::mat4_cast(rotation_);
+    }
+
+    const glm::mat4 Object::getInverseRotationMatrix()
+    {
+        return glm::mat4_cast(glm::conjugate(rotation_));
+    }
+
+    // Scale
+    void Object::setScale(glm::vec3 const &scale)
     {
         scale_ = scale;
     }
 
-    glm::vec3 Object::getScale()
+    const glm::vec3 Object::getScale()
     {
         return scale_;
+    }
+
+    const glm::mat4 Object::getScaleMatrix()
+    {
+        return glm::scale(glm::mat4(1.0f), scale_);
+    }
+
+    const glm::mat4 Object::getInverseScaleMatrix()
+    {
+        return glm::scale(glm::mat4(1.0f), 1.0f / scale_);
+    }
+
+    // Transformation
+
+    const glm::mat4 Object::getTransformMatrix()
+    {
+        return getTranslationMatrix() * getRotationMatrix() * getScaleMatrix();
+    }
+
+    const glm::mat4 Object::getInverseTransformMatrix()
+    {
+        return getInverseScaleMatrix() * getInverseRotationMatrix()
+            * getInverseTranslationMatrix();
     }
 
 } // namespace Playground::Core
