dc9e86945ae04635d7ade0a518617520f357aa03
Damien DIDIER <damien.didier@epita.fr>
2023-10-24 18:41:15 +0200

    Update object testsuit

--- tests/test_objects.cc
+++ tests/test_objects.cc
@@ -1,13 +1,49 @@
-#include <check.h>
 #include <cstdlib>
-#include <glm/ext/matrix_float4x4.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
+
+extern "C"
+{
+#include <check.h>
+}
 
 #include "core/objets/object.hh"
-START_TEST(test_test)
+
+START_TEST(objects_origin)
 {
     Playground::Core::Object obj{};
-    glm::mat4 t = obj.getTransformMatrix();
+    glm::mat4 t = obj.getModelMatrix();
     ck_assert(t == glm::mat4(1.0f));
+}
+END_TEST
+
+START_TEST(object_constuctor_position_vec3)
+{
+    Playground::Core::Object obj({ 1, 1, 1 }, { 1, 0, 0, 0 }, { 1, 1, 1 });
+    glm::vec3 position = obj.getPosition();
+    ck_assert(position == glm::vec3(1, 1, 1));
+}
+END_TEST
+
+START_TEST(object_constuctor_position_mat4)
+{
+    Playground::Core::Object obj({ 1, 1, 1 }, { 1, 0, 0, 0 }, { 1, 1, 1 });
+    glm::mat4 modelMatrix = obj.getModelMatrix();
+    glm::mat4 ref = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1);
+
+    ck_assert(modelMatrix == ref);
+}
+END_TEST
+
+START_TEST(object_set_position_vec3)
+{
+    Playground::Core::Object obj{};
+    obj.setPosition({ 1, 1, 1 });
+
+    glm::vec3 position = obj.getPosition();
+    glm::vec3 ref = { 1, 1, 1 };
+
+    ck_assert(position == ref);
 }
 END_TEST
 
@@ -15,11 +51,19 @@ Suite *test_objects_suite(void)
 {
     Suite *s;
     TCase *tc_core;
+    TCase *tc_position;
     s = suite_create("Objects");
-    tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, test_test);
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, objects_origin);
     suite_add_tcase(s, tc_core);
+
+    tc_position = tcase_create("Position");
+    tcase_add_test(tc_position, object_constuctor_position_vec3);
+    tcase_add_test(tc_position, object_constuctor_position_mat4);
+    tcase_add_test(tc_position, object_set_position_vec3);
+    suite_add_tcase(s, tc_position);
+
     return s;
 }
 
