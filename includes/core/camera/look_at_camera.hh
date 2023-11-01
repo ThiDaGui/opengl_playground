8491d998d8a5f87c0d9e490718d826ddf378c5ba
Damien DIDIER <damien.didier@epita.fr>
2023-11-01 13:01:22 +0100

    Create LookAtCamera class

---
+++ includes/core/camera/look_at_camera.hh
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <glm/ext/matrix_float4x4.hpp>
+#include <glm/ext/vector_float3.hpp>
+
+#include "core/camera/camera.hh"
+
+namespace Playground::Core
+{
+
+    class LookAtCamera : public Camera
+    {
+    private:
+        glm::vec3 up_;
+        glm::vec3 position_;
+        glm::vec3 look_at_;
+
+    public:
+        LookAtCamera();
+        LookAtCamera(glm::vec3 up);
+        LookAtCamera(glm::vec3 up, glm::vec3 position, glm::vec3 look_at);
+
+        virtual const glm::mat4 getViewMatrix() override;
+        void setViewMatrix(const glm::vec3 position, const glm::vec3 look_at);
+
+        const glm::vec3 getPosition();
+        void setPosition(const glm::vec3 position);
+
+        const glm::vec3 getLookAt();
+        void setLookAt(const glm::vec3 look_at);
+    };
+
+} // namespace Playground::Core
---
+++ src/core/camera/look_at_camera.cc
@@ -0,0 +1,60 @@
+#include "core/camera/look_at_camera.hh"
+
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/vector_float3.hpp>
+#include <glm/fwd.hpp>
+
+namespace Playground::Core
+{
+    LookAtCamera::LookAtCamera()
+        : up_(0, 0, 1)
+        , position_(0, 0, 0)
+        , look_at_(1, 0, 0)
+    {}
+
+    LookAtCamera::LookAtCamera(glm::vec3 up)
+        : up_(up)
+        , position_(0, 0, 0)
+        , look_at_(1, 0, 0)
+    {}
+
+    LookAtCamera::LookAtCamera(glm::vec3 up, glm::vec3 position,
+                               glm::vec3 look_at)
+        : up_(up)
+        , position_(position)
+        , look_at_(look_at)
+    {}
+
+    const glm::mat4 LookAtCamera::getViewMatrix()
+    {
+        return glm::lookAt(position_, look_at_, up_);
+    }
+
+    void LookAtCamera::setViewMatrix(const glm::vec3 position,
+                                     const glm::vec3 look_at)
+    {
+        position_ = position;
+        look_at_ = look_at;
+    }
+
+    const glm::vec3 LookAtCamera::getPosition()
+    {
+        return position_;
+    }
+
+    void LookAtCamera::setPosition(const glm::vec3 position)
+    {
+        position_ = position;
+    }
+
+    const glm::vec3 LookAtCamera::getLookAt()
+    {
+        return look_at_;
+    }
+
+    void LookAtCamera::setLookAt(const glm::vec3 look_at)
+    {
+        look_at_ = look_at;
+    }
+
+} // namespace Playground::Core
