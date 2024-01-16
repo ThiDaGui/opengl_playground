02758a358e9483305a5a29ef9266da405d44acf4
Damien DIDIER <damien.didier@epita.fr>
2024-01-16 15:44:10 +0100

    add shader structs

---
+++ includes/core/program/shader_structs.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glm/matrix.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+namespace Playground::Shaders
+{
+
+using namespace glm;
+
+#include <structs.glsl>
+
+} // namespace Playground::Shaders
---
+++ shaders/structs.glsl
@@ -0,0 +1,10 @@
+struct CameraData {
+    mat4 view;
+    mat4 proj;
+    mat4 view_proj;
+};
+
+struct FrameData {
+    CameraData camera;
+};
+
